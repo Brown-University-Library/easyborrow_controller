17a2d7993326e31e0b450047481fc30385b4fa13
birkin <birkin.diana@gmail.com>
2015-06-05 12:02:58 -0400

    passes test.

--- classes/web_logger.py
+++ classes/web_logger.py
@@ -10,25 +10,28 @@ class WebLogger( object ):
         This enables a request to be easily tracked through a web-admin interface. """
 
     def __init__( self, logger ):
-        self.WEBLOG_URL = unicode( os.environ['ezbCTL__LOG_URL'] )
-        self.WEBLOG_KEY = unicode( os.environ['ezbCTL__LOG_KEY'] )
-        self.WEBLOGENTRY_MINIMUM_IMPORTANCE_LEVEL = unicode( os.environ['ezbCTL__LOGENTRY_MINIMUM_IMPORTANCE_LEVEL'] )
+        self.WEBLOG_URL = unicode( os.environ['ezbCTL__WEBLOG_URL'] )
+        self.WEBLOG_KEY = unicode( os.environ['ezbCTL__WEBLOG_KEY'] )
+        self.WEBLOGENTRY_MINIMUM_IMPORTANCE_LEVEL = unicode( os.environ['ezbCTL__WEBLOGENTRY_MINIMUM_IMPORTANCE_LEVEL'] )
+        self.logger = logger
 
-    def post_message( self, message, identifier, importance,  ):
+    def post_message( self, message, identifier, importance ):
         """ Sends weblog message based on importance. """
+        status_code = None
         important_enough = self.evaluate_importance( importance )
         if important_enough:
-            self.run_post( message, identifier )
-        return
+            self.logger.debug( u'identifier, `%s`; message, `%s`; importance, `%s`' % (message, identifier, importance) )
+            status_code = self.run_post( message, identifier )
+        return status_code
 
     def evaluate_importance( self, stated_importance ):
         """ Determines whether to send message based on stated message performance vs env filter setting.
             Returns boolean.
             Called by post_message() """
         assessed_importance = False
-        if self.WEBLOGENTRY_MINIMUM_IMPORTANCE_LEVEL == u'debug'
+        if self.WEBLOGENTRY_MINIMUM_IMPORTANCE_LEVEL == u'debug':
             assessed_importance = True
-        elif self.WEBLOGENTRY_MINIMUM_IMPORTANCE_LEVEL == u'info' and stated_importance=u'info':
+        elif self.WEBLOGENTRY_MINIMUM_IMPORTANCE_LEVEL == u'info' and stated_importance == u'info':
             assessed_importance = True
         return assessed_importance
 
@@ -37,4 +40,5 @@ class WebLogger( object ):
             Called by post_message() """
         params = { 'message': message, 'identifier': identifier, 'key': self.WEBLOG_KEY }
         r = requests.post( self.WEBLOG_URL, data=params )
-        return
+        status_code = r.status_code
+        return status_code
