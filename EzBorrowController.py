0434907f1d1b765626b4fb0ac1980305a2fa5847
birkin <birkin.diana@gmail.com>
2016-01-22 15:10:51 -0500

    removes commented out code.

--- EzBorrowController.py
+++ EzBorrowController.py
@@ -169,7 +169,6 @@ class Controller( object ):
             # update 'requests' table & send email on success ('for' loop is over)
             #######
 
-            # web_logger.post_message( message='- in controller; itemInstance.requestSuccessStatus is: %s' % itemInstance.requestSuccessStatus, identifier=self.log_identifier, importance='info' )
             web_logger.post_message( message='- in controller; request_inst.current_status is: %s' % request_inst.current_status, identifier=self.log_identifier, importance='info' )
 
             if( itemInstance.requestSuccessStatus == "success" ):
@@ -186,27 +185,6 @@ class Controller( object ):
                     web_logger.post_message( message='- in controller; "blocked" detected; sendEmail() was called', identifier=self.log_identifier, importance='info' )
                     self.update_history_action( eb_request_number, 'illiad', 'followup', 'blocked_user_emailed', '' )  # request_num, service, action, result, transaction_num
                     self.update_request_status( 'illiad_block_user_emailed', request_inst.request_number )
-
-            # elif( itemInstance.requestSuccessStatus == 'login_failed_possibly_blocked' ):
-            #     if request_inst.current_status == 'login_failed_possibly_blocked' and patron_inst.eppn == 'bdiana':  # temp dev
-            #         web_logger.post_message( message='- in controller; "blocked" detected; will send user email', identifier=self.log_identifier, importance='info' )
-            #         mail_builder = MailBuilder( request_inst, patron_inst, item_inst )
-            #         mail_builder.prep_email()
-            #         mailer = Mailer( mail_builder.to, mail_builder.reply_to, mail_builder.subject, mail_builder.message, request_inst.request_number  )
-            #         if mailer.send_email() is True:
-            #             web_logger.post_message( message='- in controller; "blocked" detected; sendEmail() was called', identifier=self.log_identifier, importance='info' )
-            #             self.update_history_action( eb_request_number, 'illiad', 'followup', 'blocked_user_emailed', '' )  # request_num, service, action, result, transaction_num
-            #             self.update_request_status( 'illiad_block_user_emailed', request_inst.request_number )
-            #     else:
-            #         web_logger.post_message( message='- in controller; "blocked" detected; will send user email', identifier=self.log_identifier, importance='info' )
-            #         result_dict = utility_code.makeEbRequest( itemInstance, self.log_identifier )  # eb_request is a storage-object; NOTE: as code is migrated toward newer architecture; this line will occur near beginning of runCode()
-            #         web_logger.post_message( message='- in controller; eb_request obtained', identifier=self.log_identifier, importance='info' )
-            #         result_dict = utility_code.sendEmail( result_dict['eb_request'], self.log_identifier )
-            #         web_logger.post_message( message='- in controller; "blocked" detected; sendEmail() was called', identifier=self.log_identifier, importance='info' )
-            #         self.update_history_action( eb_request_number, 'illiad', 'followup', 'blocked_user_emailed', '' )  # request_num, service, action, result, transaction_num
-            #         #
-            #         if result_dict['status'] == 'success':
-            #             self.update_request_status( 'illiad_block_user_emailed', eb_request_number )
 
             elif( itemInstance.requestSuccessStatus == "failure_no_sfx-link_to_illiad" ):
               self.update_request_status( 'processed', eb_request_number )
