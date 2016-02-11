b399d358b46165045573cecc7fca15bb540adc9e
birkin <birkin.diana@gmail.com>
2016-02-11 15:58:04 -0500

    removes commented-out code.

--- utility_code.py
+++ utility_code.py
@@ -48,42 +48,6 @@ class EB_Request:
 
 
 
-# def evaluateIlliadSubmissionV2( itemInstance, send_result_dict, log_identifier ):
-#   try:
-#     return_dict = { 'status': 'init' }
-#     ## new-user check
-#     if 'new_user' in send_result_dict:
-#       if send_result_dict['new_user'] == True:
-#         web_logger.post_message( message='- in utility_code.evaluateIlliadSubmissionV2(); Illiad user created...', identifier=log_identifier, importance='info' )
-#         parameterDict = { 'serviceName': 'illiad', 'action': 'create_illiad_user_attempt', 'result': 'success', 'number': '' }
-#         # itemInstance.updateHistoryAction( parameterDict['serviceName'], parameterDict['action'], parameterDict['result'], parameterDict['number'] )
-#     ## illiad status check
-#     if 'status' in send_result_dict:
-#       parameterDict = None  # for history update
-#       if send_result_dict['status'] == 'submission_successful':
-#         itemInstance.illiadAssignedReferenceNumber = send_result_dict['transaction_number']  # it should be there
-#         parameterDict = { 'serviceName': 'illiad', 'action': 'attempt', 'result': 'Request_Successful', 'number': itemInstance.illiadAssignedReferenceNumber }
-#         itemInstance.requestSuccessStatus = 'success'
-#         itemInstance.genericAssignedUserEmail = itemInstance.patronEmail
-#         itemInstance.genericAssignedReferenceNumber = itemInstance.illiadAssignedReferenceNumber
-#       elif send_result_dict['status'] == 'login_failed_possibly_blocked':
-#         itemInstance.requestSuccessStatus = 'login_failed_possibly_blocked'
-#         itemInstance.genericAssignedUserEmail = itemInstance.patronEmail
-#         # patronInfo = itemInstance.grabPatronApiInfo(itemInstance.patronId)
-#         # itemInstance.grabConvertedPatronApiInfo(patronInfo) # grabs converted info and stores it to attributes
-#       if parameterDict == None:
-#         parameterDict = { 'serviceName': 'illiad', 'action': 'attempt', 'result': send_result_dict['status'], 'number': '' }
-#       # itemInstance.updateHistoryAction( parameterDict['serviceName'], parameterDict['action'], parameterDict['result'], parameterDict['number'] )
-#       return_dict['status'] = 'evaluation_successful'
-#     web_logger.post_message( message='- in utility_code.evaluateIlliadSubmissionV2(); return_dict: %s' % unicode(repr(return_dict)), identifier=log_identifier, importance='info' )
-#     return return_dict
-#   except:
-#     message = '- in utility_code.evaluateIlliadSubmissionV2(); error detail: %s' % makeErrorString()
-#     web_logger.post_message( message=message, identifier=log_identifier, importance='info' )
-#     return { 'error_message': message }
-
-
-
 def jsonify(structure, rq_indent=2):
   '''
   - Purpose: serializes and formats non-model object in json.
@@ -92,40 +56,6 @@ def jsonify(structure, rq_indent=2):
   data = json.dumps( structure, sort_keys=True, indent=rq_indent )
   return data
   # end def jsonify()
-
-
-
-# def makeEbRequest( itemInstance, log_identifier ):
-#   '''
-#   - Purpose: temporary transitional hack while new controller architecture is slowly implemented; converts itemInstance to an EB_Request instance.
-#   - Called by: controller.runCode()
-#   '''
-#   try:
-#     eb_request = EB_Request()
-#     eb_request.patron_firstname = itemInstance.firstname
-#     eb_request.patron_lastname = itemInstance.lastname
-#     eb_request.patron_email = itemInstance.patronEmail
-#     eb_request.patron_phone = itemInstance.patron_api_telephone
-#     eb_request.patron_address = itemInstance.patron_api_address
-#     eb_request.patron_status = itemInstance.patronStatus
-#     eb_request.patron_department = itemInstance.patron_api_dept
-#     #
-#     eb_request.item_title = itemInstance.itemTitle
-#     eb_request.item_oclc_number = itemInstance.oclcNumber
-#     #
-#     eb_request.request_ezb_reference_number = log_identifier
-#     eb_request.request_current_tunneler_service = itemInstance.currentlyActiveService
-#     eb_request.request_current_tunneler_status = itemInstance.requestSuccessStatus
-#     #
-#     logger.debug( 'id, `%s`; eb_request.__dict__ is: %s' % (log_identifier, eb_request.__dict__) )
-#     return_dict = { 'status': 'success', 'eb_request': eb_request }
-#     logger.debug( 'id, `%s`; return_dict is: %s' % (log_identifier, pprint.pformat(return_dict)) )
-#     return return_dict
-#   except:
-#     message = '- in utility_code.makeEbRequest(); error detail: %s' % makeErrorString()
-#     web_logger.post_message( message='- in utility_code.makeEbRequest(); exception, `%s`' % message, identifier=log_identifier, importance='error' )
-#     return { 'status': 'failure', 'message': message }
-#   # end def makeEbRequest()
 
 
 
@@ -258,58 +188,3 @@ Once your account issue is cleared up, click on this link to re-request the item
     web_logger.post_message( message=message, identifier=log_identifier, importance='error' )
     return { 'status': 'failure', 'message': message }
   # end def sendEmailIlliadBlocked()
-
-
-
-# def submitIlliadRequest( parameter_dict, log_identifier ):
-#   '''
-#   - Purpose: hits the django illiad submission web-service.
-#   - Called by: controller.runCode()
-#   - Usage: see tests.doc_tests.test_submitIlliadRequest()
-#   '''
-
-#   try:
-
-#     web_logger.post_message( message='- in utility_code.submitIlliadRequest(); parameter_dict is: %s' % pprint.pformat(parameter_dict), identifier=log_identifier, importance='info' )
-
-#     data = urllib.urlencode( parameter_dict )
-#     request = urllib2.Request( settings.ILLIAD_REQUEST_URL, data )
-#     response = urllib2.urlopen( request )
-#     json_string = response.read()
-#     json_dict = json.loads( json_string )
-#     web_logger.post_message( message='- in utility_code.submitIlliadRequest(); submission result is: %s' % pprint.pformat(json_dict), identifier=log_identifier, importance='info' )
-#     return json_dict
-
-#   except:
-#     message = '- in utility_code.submitIlliadRequest(); error detail: %s' % makeErrorString()
-#     web_logger.post_message( message=message, identifier=log_identifier, importance='error' )
-#     return { 'status': 'failure', 'message': message }
-
-#   # end def submitIlliadRequest()
-
-
-
-# def submitIlliadRemoteAuthRequestV2( parameter_dict, log_identifier ):
-#   try:
-#     url = settings.ILLIAD_API_URL
-#     headers = { 'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8' }
-#     r = requests.post( url, data=parameter_dict, headers=headers, timeout=60, verify=False )
-#     logger.debug( 'id, `%s`; ws response text, ```%s```' % (log_identifier, r.text) )
-#     return_dict = json.loads( r.text )
-#     web_logger.post_message( message='- in utility_code.submitIlliadRequestV2(); return_dict: %s' % pprint.pformat(return_dict), identifier=log_identifier, importance='info' )
-#     return return_dict
-#   except:
-#     message = '- in utility_code.submitIlliadRequestV2(); error detail: %s' % makeErrorString()
-#     web_logger.post_message( message=message, identifier=log_identifier, importance='error' )
-#     return { 'error_message': message }
-#   # end def submitIlliadRemoteAuthRequestV2()
-
-
-
-# def jsonify_db_data( data_dict ):
-#   """ Returns json string for given tuple of row-dict entries.
-#       Allows result to be logged easily.
-#       Called by ezb_controller.py """
-#   data_dict['created'] = unicode( data_dict['created'] )
-#   jstring = json.dumps( data_dict, sort_keys=True, indent=2 )
-#   return jstring
