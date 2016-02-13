1157fd7fd438ff9ad23c7ceb9fb4136bebaba9b4
birkin <birkin.diana@gmail.com>
2016-02-13 09:27:13 -0500

    updates test; passes.

--- tests/db_tests.py
+++ tests/db_tests.py
@@ -19,14 +19,9 @@ dbh = db_handler.Db_Handler( logger )
 class Db_HandlerTest(unittest.TestCase):
 
     def test__unicodify_resultset_unicode(self):
-        dict_list = [ {'title': 'réd' } ]
-        self.assertNotEqual( [ {'title': 'réd' } ], dbh._unicodify_resultset(dict_list) )
-        self.assertEqual( [ {'title': 'réd' } ], dbh._unicodify_resultset(dict_list) )
-
-    def test__unicodify_resultset_unicode(self):
-        dict_list = [ {'title': 'réd', 'wc': long( 123 ) } ]
+        """ Checks that all returned values from db call are unicode strings. """
+        dict_list = [ {'title'.encode('utf-8'): 'réd'.encode('utf-8'), 'wc'.encode('utf-8'): long( 123 ) } ]
         self.assertEqual( [ {'title': 'réd', 'wc': '123' } ], dbh._unicodify_resultset(dict_list) )
-
 
 
 
