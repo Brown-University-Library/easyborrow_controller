e8555142a0f75c346d2508e44caca40053b0c4fd
birkin <birkin.diana@gmail.com>
2017-09-19 14:21:50 -0400

    stubs bd-caller test.

--- test.py
+++ test.py
@@ -16,9 +16,9 @@ import logging, os, sys, unittest
 parent_working_dir = os.path.abspath( os.path.join(os.getcwd(), os.pardir) )
 sys.path.append( parent_working_dir )
 
+from easyborrow_controller_code.tests import bd_caller_tests
 from easyborrow_controller_code.tests import emailer_tests
 from easyborrow_controller_code.tests import illiad_caller_tests
-# from easyborrow_controller_code.tests import tunneler_runner_tests
 
 
 logging.basicConfig(
@@ -31,9 +31,9 @@ logger = logging.getLogger(__name__)
 
 def suite():
     test_suite = unittest.TestSuite()
+    test_suite.addTest( bd_caller_tests.suite() )  # stub only, TODO make tests
     test_suite.addTest( emailer_tests.suite() )
     test_suite.addTest( illiad_caller_tests.suite() )
-    # test_suite.addTest( tunneler_runner_tests.suite() )
     return test_suite
 
 
--- tests/bd_caller_tests.py
+++ tests/bd_caller_tests.py
@@ -2,9 +2,16 @@
 
 from __future__ import unicode_literals
 
-import datetime, json, unittest
-from easyborrow_controller_code.classes.tunneler_runners import BD_ApiRunner, IlliadApiRunner
-from easyborrow_controller_code.classes.basics import Request_Meta
+import datetime, json, logging, unittest
+from easyborrow_controller_code.classes.tunneler_runners import BD_ApiRunner
+
+
+logging.basicConfig(
+    filename='',
+    level=logging.DEBUG,
+    format='[%(asctime)s] %(levelname)s [%(module)s-%(funcName)s()::%(lineno)d] %(message)s',
+    datefmt='%d/%b/%Y %H:%M:%S' )
+logger = logging.getLogger(__name__)
 
 
 # class BDRunnerTest(unittest.TestCase):
@@ -203,67 +210,29 @@ from easyborrow_controller_code.classes.basics import Request_Meta
 #     assert d['response']['search_result'] == 'FAILURE', d['response']['search_result']
 
 
-# class BDRunnerTest(unittest.TestCase):
-
-#     def setUp(self):
-#         self.bd_runner = BD_ApiRunner()
-#         pass
-
-#     def tearDown(self):
-#         pass
-
-#     def test_prepare_params(self):
-#         patron_inst = 'x'
-#         item_inst = 'y'
-#         self.assertEqual( 'bar', self.bd_runner.prepare_params(patron_inst, item_inst) )
-
-#     ## end class BDRunnerTest()
-
-
-class IlliadApiRunnerTest(unittest.TestCase):
+class BDRunnerTest(unittest.TestCase):
 
     def setUp(self):
-        self.illiad_runner = self.instantiate_illiad_runner()
+        self.bd_runner = BD_ApiRunner( logger=logger, log_identifier='foo' )
         pass
-
-    def instantiate_illiad_runner(self):
-        """ Sets up IlliadApiRunner instance.
-            Called by setUp() """
-        request_instance = Request_Meta()
-        request_instance.request_number = 42
-        illiad_runner = IlliadApiRunner( request_instance )
-        return illiad_runner
 
     def tearDown(self):
         pass
 
-    def test_make_openurl_segment__simple_case(self):
-        volumes_info = ''
-        initial_url = 'http://rl3tp7zf5x.search.serialssolutions.com/?sid=test&genre=book&isbn=9780439339117'
-        self.assertEqual( 'sid=test&genre=book&isbn=9780439339117', self.illiad_runner._make_openurl_segment(initial_url, volumes_info) )
+    # def test_prepare_params(self):
+    #     patron_inst = 'x'
+    #     item_inst = 'y'
+    #     self.assertEqual( 'bar', self.bd_runner.prepare_params(patron_inst, item_inst) )
 
-    def test_make_openurl_segment__unknown_genre(self):
-        volumes_info = ''
-        initial_url = 'http://rl3tp7zf5x.search.serialssolutions.com/?sid=test&genre=unknown&isbn=9780439339117'
-        self.assertEqual( 'sid=test&genre=book&isbn=9780439339117', self.illiad_runner._make_openurl_segment(initial_url, volumes_info) )
+    def test_this_test_harness(self):
+        self.assertEqual( 1, 1 )
 
-    def test_make_openurl_segment__volume_note(self):
-        volumes_info = 'test note'
-        initial_url = 'http://rl3tp7zf5x.search.serialssolutions.com/?sid=test&genre=unknown&isbn=9780439339117'
-        self.assertEqual( 'sid=test&genre=book&isbn=9780439339117&notes=test+note', self.illiad_runner._make_openurl_segment(initial_url, volumes_info) )
-
-    def test_make_openurl_segment__unicode_volume_note(self):
-        volumes_info = 'tést note'
-        initial_url = 'http://rl3tp7zf5x.search.serialssolutions.com/?sid=test&genre=unknown&isbn=9780439339117'
-        self.assertEqual( 'sid=test&genre=book&isbn=9780439339117&notes=t%C3%A9st+note', self.illiad_runner._make_openurl_segment(initial_url, volumes_info) )
-
-    ## end class IlliadApiRunnerTest()
+    ## end class BDRunnerTest()
 
 
 def suite():
     suite = unittest.TestSuite()
-    # suite.addTest( unittest.makeSuite(BDRunnerTest) )
-    suite.addTest( unittest.makeSuite(IlliadApiRunnerTest) )
+    suite.addTest( unittest.makeSuite(BDRunnerTest) )
     return suite
 
 
