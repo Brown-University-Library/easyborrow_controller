c8ea5cf9a3d22e1cefdf0b3d87b536bf77afd4ad
birkin <birkin.diana@gmail.com>
2017-10-31 11:26:32 -0400

    removes logger attribute.

--- classes/bd_caller_exact.py
+++ classes/bd_caller_exact.py
@@ -24,7 +24,7 @@ class BD_CallerExact( object ):
     def __init__( self ):
         """ Loads env vars.
             Called by EzBorrowController.py """
-        self.logger = logger
+        # self.logger = logger
         self.db_handler = None
         self._prep_db_handler()
         self.log_identifier = 'init'
@@ -46,12 +46,12 @@ class BD_CallerExact( object ):
         self.db_handler = Db_Handler( log )
         return
 
-    def setup_api_hit( self, item_inst, web_logger ):
+    def setup_api_hit( self, item_inst ):
         """ Sets the currently-active-service and updates weblog.
             Called by controller.run_code() """
         item_inst.current_service = 'borrowDirect'
-        web_logger.post_message( message='- in controller; checking BorrowDirect...', identifier=self.log_identifier, importance='info' )
-        self.logger.debug( '- identifier, %s; setup_api_hit() complete' % self.log_identifier )
+        # web_logger.post_message( message='- in controller; checking BorrowDirect...', identifier=self.log_identifier, importance='info' )
+        log.debug( '- identifier, %s; setup_api_hit() complete' % self.log_identifier )
         return item_inst
 
     def prepare_params( self, patron_inst, item_inst ):
@@ -63,14 +63,14 @@ class BD_CallerExact( object ):
     def hit_bd_api( self, isbn, user_barcode ):
         """ Handles bdpyweb call.
             Called by Controller.run_code() """
-        self.logger.info( '%s- starting try_request()' % self.log_identifier )
+        log.info( '%s- starting try_request()' % self.log_identifier )
         parameter_dict = self.prepare_bd_api( isbn, user_barcode )
         try:
             r = requests.post( self.bdpyweb_defaults['url'], data=parameter_dict, timeout=300, verify=False )
-            self.logger.debug( '%s- bdpyweb response content, `%s`' % (self.log_identifier, r.content.decode('utf-8')) )
+            log.debug( '%s- bdpyweb response content, `%s`' % (self.log_identifier, r.content.decode('utf-8')) )
             self.api_result = json.loads( r.content )
         except Exception, e:
-            self.logger.debug( '%s- exception on bdpyweb post, `%s`' % (self.log_identifier, pprint.pformat(unicode(repr(e)))) )
+            log.debug( '%s- exception on bdpyweb post, `%s`' % (self.log_identifier, pprint.pformat(unicode(repr(e)))) )
         return
 
     def prepare_bd_api( self, isbn, user_barcode ):
@@ -81,7 +81,7 @@ class BD_CallerExact( object ):
             'api_identity': self.bdpyweb_defaults['api_identity'],
             'isbn': isbn,
             'user_barcode': user_barcode }
-        self.logger.debug( '%s- post parameter_dict, `%s`' % (self.log_identifier, parameter_dict) )
+        log.debug( '%s- post parameter_dict, `%s`' % (self.log_identifier, parameter_dict) )
         return parameter_dict
 
     def process_response( self ):
@@ -92,7 +92,7 @@ class BD_CallerExact( object ):
                 self.api_confirmation_code = self.api_result['bd_confirmation_code']
                 self.api_found = True
                 self.api_requestable = True
-        self.logger.debug( '%s- process_response complete; code, `%s`; found, `%s`; requestable, `%s`' % (self.log_identifier, self.api_confirmation_code, self.api_found, self.api_requestable) )
+        log.debug( '%s- process_response complete; code, `%s`; found, `%s`; requestable, `%s`' % (self.log_identifier, self.api_confirmation_code, self.api_found, self.api_requestable) )
         return
 
     def update_history_table( self ):
@@ -101,8 +101,8 @@ class BD_CallerExact( object ):
         ( api_confirmation_code, history_table_message ) = self.prep_code_message()
         utf8_sql = self.prep_history_sql( api_confirmation_code, history_table_message )
         self.db_handler.run_sql( utf8_sql )
-        self.web_logger.post_message( message='- in tunneler_runners.BD_ApiRunner.update_history_table(); history table updated for ezb#: %s' % self.log_identifier, identifier=self.log_identifier, importance='info' )
-        self.logger.debug( 'update_history_table complete' )
+        # self.web_logger.post_message( message='- in tunneler_runners.BD_ApiRunner.update_history_table(); history table updated for ezb#: %s' % self.log_identifier, identifier=self.log_identifier, importance='info' )
+        log.debug( 'update_history_table complete' )
         return
 
     def prep_code_message( self ):
@@ -114,7 +114,7 @@ class BD_CallerExact( object ):
         else:
             api_confirmation_code = ''
             history_table_message = 'not_requestable'
-        self.logger.debug( '%s- prep_code_message complete; local api_confirmation_code, `%s`; history_table_message, `%s`' % (self.log_identifier, api_confirmation_code, history_table_message) )
+        log.debug( '%s- prep_code_message complete; local api_confirmation_code, `%s`; history_table_message, `%s`' % (self.log_identifier, api_confirmation_code, history_table_message) )
         return ( api_confirmation_code, history_table_message )
 
     def prep_history_sql( self, api_confirmation_code, history_table_message ):
@@ -129,7 +129,7 @@ class BD_CallerExact( object ):
             api_confirmation_code
         )
         utf8_sql = sql.encode( 'utf-8' )  # old code expects utf-8 string
-        self.logger.debug( '%s- prep_history_sql complete; utf8_sql, `%s`' % (self.log_identifier, sql.decode('utf-8')) )
+        log.debug( '%s- prep_history_sql complete; utf8_sql, `%s`' % (self.log_identifier, sql.decode('utf-8')) )
         return utf8_sql
 
     def handle_success( self, request_inst ):
